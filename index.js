a9496ed90c5f2639bde2a82967956b6cd73873b6
Terence Holmes <piece4ulmind@gmail.com>
2022-04-09 20:58:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching (drivers,query) {
+
+return drivers.filter(function (callback) { 
+    return callback.toLowerCase() === query.toLowerCase() })   
+}
+
+function fuzzyMatch (drivers,query) {
+
+    return drivers.filter(function (callback) {
+        return callback.toLowerCase().slice(0,query.length) === query.toLowerCase()
+    })
+}
+
+function matchName (drivers,query) {
+
+    return drivers.filter(function(callback) {
+        return callback.name === query
+    })
+}
